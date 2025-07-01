26c8f053b80553b9bba46c3e68887e0a508de539
bruno-gabriel-muniz <brunomunizlourenco@gmail.com>
2025-07-01 19:23:20 -0300

    test: teste do método que cria a conta.

---
+++ Tests/TestAuthenticationSer.cpp
@@ -0,0 +1,46 @@
+#include "Source/Service/AuthenticationSer.cpp"
+#include "Tests/Test.hpp"
+
+class TestMakeAccount : public Test{
+    public:
+        void exec(){
+            // Definindo os dados do teste.
+            nameTest = "Teste->AuthentCommandMakeAccount->execute";
+            typeTest = ValidArgument();
+            in = "Cpf: 842.259.180-41 | Senha: B1g#ji | Nome: Ze de Fulano";
+            expected = "void";
+
+            // Criando as variáveis do teste
+            CtrState *contexto = new CtrState();
+            AuthenticationSer ctrAuthen = AuthenticationSer(contexto);
+            Ncpf cpf;
+            Senha senha;
+            Nome nome;
+            cpf.setValor("842.259.180-41");
+            senha.setValor("B1g#ji");
+            nome.setValor("Ze de Fulano");
+
+            // Rodando o teste
+            try{
+                ctrAuthen.makeAccount(cpf, senha, nome);
+            } catch (runtime_error &x) {
+                out = x.what();
+                result = ResultFail();
+            }
+
+            // Verificando os dados;
+            Conta *userContext = contexto->getUser();
+
+            if(userContext->getNcpf().getValor() == cpf.getValor() and
+                userContext->getSenha().getValor() == senha.getValor() and
+                userContext->getNome().getValor() == nome.getValor())
+            {
+                out = "void";
+                result = ResultPass();
+            } else {
+                out = "void, but value error.";
+                result = ResultFail();
+            };
+            delete contexto;
+        }
+};
--- Tests/TestMain.cpp
+++ Tests/TestMain.cpp
@@ -1,5 +1,7 @@
 #include "Tests/TestCtrState.cpp"
+#include "Tests/TestAuthenticationSer.cpp"
 
 int main(){
     TestCtrStateRun().run();
+    TestMakeAccount().run();
 }
