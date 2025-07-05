ce11fdf2ff67d1b3b06f7336afb6e8f1312823c0
bruno-gabriel-muniz <brunomunizlourenco@gmail.com>
2025-07-05 12:52:59 -0300

    feat: makeOrder adicionado

--- Source/Service/InvestmentSer.cpp
+++ Source/Service/InvestmentSer.cpp
@@ -12,6 +12,30 @@ void InvestmentCommandRemoveOrder::execute(CodigoNeg codNeg){
 
 // TODO:
 void InvestmentCommandMakeOrder::execute(CodigoNeg codNeg, Quantidade quantidade, Data data){
+    // Inicia o DB;
+    DB *db = DB::getInstance();
+    Tabela *resultSql = new Tabela();
+
+    // Dados do contexto
+    Carteira *userCarteira = contexto->getCarteira();
+
+    // Encontra a ordem no dados históricos;
+    string findOrdem = 
+        "SELECT PRECO FROM DadosHistoricos WHERE DATA = \"" +
+        data.getValor() + "\", \"CODIGO NEG\" = \"" + codNeg.getValor() + "\";";
+    db->exec(findOrdem, resultSql, "Erro ao procurar a folha nos dados históricos: ");
+
+    // Valida a ordem;
+    if(resultSql->size() > 1) throw runtime_error("Mais de uma folha encontrada.");
+    else if(resultSql->size() == 0) throw runtime_error("Nenhuma folha encontrada.");
+
+    // Cria a ordem;
+    string makeOrder =
+        "INSERT INTO Ordens (CODIGO, \"CODIGO NEG\", DATA, PRECO, QUANTIDADE) VALUES (\"" +
+        userCarteira->getCodigo().getValor() + "\", \"" + codNeg.getValor() + "\", \"" +
+        data.getValor() + "\", \"" + resultSql[0][0]["PRECO"] + "\", \"" + quantidade.getValor() +
+        "\")";
+    db->exec(makeOrder, resultSql, "Erro ao adicionar a nova ordem no DB: ");
     return ;
 }
 
