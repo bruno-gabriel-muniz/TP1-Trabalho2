cef63cd1ff5847bf6e23165e1603d2aaa70bf555
bruno-gabriel-muniz <brunomunizlourenco@gmail.com>
2025-07-14 16:14:47 -0300

    refactor: UpdateDadosHistoricos refatorado para melhoria da eficiencia e descarte de Excesso de dados.

--- Source/UpdateDadosHistoricos.cpp
+++ Source/UpdateDadosHistoricos.cpp
@@ -1,4 +1,5 @@
 # include <fstream>
+# include <unordered_set>
 
 # include "DB.cpp"
 
@@ -8,21 +9,41 @@ int main(){
     Tabela *resultSql = new Tabela();
 
     // Abre o arquivo;
-    ifstream Arquivo("Source/DADOS_HISTORICOS.txt");
+    ifstream Arquivo("DADOS_HISTORICOS.txt");
 
     // Comandos SQL
     string insertDado, CodigoNeg, Data, Preco, linha;
+
+    // Codigos escolhidos
+    unordered_set<string> codigos;
+
+    // insere eles no set
+    codigos.insert("AALR3       ");
+    codigos.insert("SAPR11F     ");
+    codigos.insert("HYPE3       ");
+    codigos.insert("JBSS3       ");
+    codigos.insert("MLAS3       ");
+    codigos.insert("MRFG3       ");
+    codigos.insert("REIT11      ");
+    codigos.insert("MNDL3       ");
+    codigos.insert("CRPG5       ");
+    codigos.insert("ABCB4       ");
 
     while (getline(Arquivo, linha)){
         // Coleta os dados da Ordem;
         CodigoNeg = linha.substr(12, 12);
         Data = linha.substr(2, 8);
         Preco = linha.substr(113, 13);
-        // Insere no DB
-        insertDado =
-            "INSERT INTO DadosHistoricos (\"CODIGO NEG\", DATA, PRECO) VALUES (\"" +
-            CodigoNeg + "\", " + Data + ", " + Preco + ");";
-        db->exec(insertDado, resultSql, "Erro ao inserir um dado histórico: ");
+        
+        // Verifica se a folha eh permitida
+        if(codigos.count(CodigoNeg)){
+            // Insere no DB
+            insertDado =
+                "INSERT INTO DadosHistoricos (\"CODIGO NEG\", DATA, PRECO) VALUES (\"" +
+                CodigoNeg + "\", " + Data + ", " + Preco + ");";
+            
+            db->exec(insertDado, resultSql, "Erro ao inserir um dado histórico: ");
+        }
     }
     return 0;
 }
